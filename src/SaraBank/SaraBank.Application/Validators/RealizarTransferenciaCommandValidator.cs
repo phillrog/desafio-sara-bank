1769c68383a12c77e96964fa9f1309003f6b4294
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2026-01-08 16:20:09 -0300

    implementação da validação transferencia

---
+++ src/SaraBank/SaraBank.Application/Validators/RealizarTransferenciaCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SaraBank.Application.Commands;
+
+namespace SaraBank.Application.Validators;
+
+public class RealizarTransferenciaCommandValidator : AbstractValidator<RealizarTransferenciaCommand>
+{
+    public RealizarTransferenciaCommandValidator()
+    {
+        RuleFor(x => x.ContaOrigemId)
+            .NotEmpty().WithMessage("A conta de origem é obrigatória.");
+
+        RuleFor(x => x.ContaDestinoId)
+            .NotEmpty().WithMessage("A conta de destino é obrigatória.")
+            .NotEqual(x => x.ContaOrigemId).WithMessage("A conta de destino não pode ser igual à conta de origem.");
+
+        RuleFor(x => x.Valor)
+            .GreaterThan(0).WithMessage("O valor da transferência deve ser maior que zero.");
+    }
+}
---
+++ src/SaraBank/SaraBank.UnitTests/Validators/RealizarTransferenciaCommandValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation.TestHelper;
+using SaraBank.Application.Commands;
+using SaraBank.Application.Validators;
+
+namespace SaraBank.UnitTests.Application.Validators;
+
+public class RealizarTransferenciaCommandValidatorTests
+{
+    private readonly RealizarTransferenciaCommandValidator _validator;
+
+    public RealizarTransferenciaCommandValidatorTests()
+    {
+        _validator = new RealizarTransferenciaCommandValidator();
+    }
+
+    [Fact]
+    public void Deve_Ter_Erro_Quando_Valor_For_Zero_Ou_Negativo()
+    {
+        // Arrange
+        var command = new RealizarTransferenciaCommand("CONTA-ORIGEM", "CONTA-DESTINO", 0m);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Valor)
+              .WithErrorMessage("O valor da transferência deve ser maior que zero.");
+    }
+
+    [Fact]
+    public void Deve_Ter_Erro_Quando_Contas_Origem_E_Destino_Forem_Iguais()
+    {
+        // Arrange
+        var contaId = "CONTA-123";
+        var command = new RealizarTransferenciaCommand(contaId, contaId, 150.00m);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ContaDestinoId)
+              .WithErrorMessage("A conta de destino não pode ser igual à conta de origem.");
+    }
+
+    [Fact]
+    public void Deve_Ter_Erro_Quando_Campos_Obrigatorios_Estiverem_Vazios()
+    {
+        // Arrange
+        var command = new RealizarTransferenciaCommand("", "", 10.00m);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ContaOrigemId);
+        result.ShouldHaveValidationErrorFor(x => x.ContaDestinoId);
+    }
+
+    [Fact]
+    public void Nao_Deve_Ter_Erro_Quando_Comando_For_Valido()
+    {
+        // Arrange
+        var command = new RealizarTransferenciaCommand("CONTA-A", "CONTA-B", 250.75m);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
